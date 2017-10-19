37544a3b0f2836e4614f179562215570d9c5913f
soaryang <longfei19880>
2017-10-19 11:31:47 +0800

    commit

--- pages/sixth/sixth.js
+++ pages/sixth/sixth.js
@@ -23,31 +23,49 @@ var _songsList=[{
     name: '告白气球',
     mid: "003OUlho2HcRHC",
     singer:'周杰伦',
-    coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R90x90M000003RMaRI1iFoYd.jpg'
+    coverImgUrl: '../../image/dxx.png'
 },{
     dataUrl:'http://stream.qqmusic.tc.qq.com/138549169.mp3',
     name: '你还要我怎样',
     mid: "000E62Tc3bMiJB",
     singer:'薛之谦',
-    coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R90x90M000000aWdOx24i3dG.jpg'
+    coverImgUrl: '../../image/zzx.jpg'
 },{
     dataUrl:'http://stream.qqmusic.tc.qq.com/137903929.mp3',
     name: '微微一笑很倾城',
     mid: "002NbtFA3fuJhD",
     singer:'杨洋',
-    coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R90x90M000003RxTdZ0sJLwo.jpg'
+    coverImgUrl: '../../image/xxtq.png'
 },{
     dataUrl:'http://stream.qqmusic.tc.qq.com/132636799.mp3',
     name: '演员',
     mid: "001Qu4I30eVFYb",
     singer:'薛之谦',
-    coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R90x90M000003y8dsH2wBHlo.jpg'
+    coverImgUrl: '../../image/frjyd.png'
 }, {
   dataUrl: 'http://stream.qqmusic.tc.qq.com/132636799.mp3',
   name: '演员',
   mid: "001Qu4I30eVFYb",
   singer: '薛之谦',
-  coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R90x90M000003y8dsH2wBHlo.jpg'
+  coverImgUrl: '../../image/dxx.png'
+}, {
+  dataUrl: 'http://stream.qqmusic.tc.qq.com/137903929.mp3',
+  name: '微微一笑很倾城',
+  mid: "002NbtFA3fuJhD",
+  singer: '杨洋',
+  coverImgUrl: '../../image/dxx.png'
+}, {
+  dataUrl: 'http://stream.qqmusic.tc.qq.com/132636799.mp3',
+  name: '演员',
+  mid: "001Qu4I30eVFYb",
+  singer: '薛之谦',
+  coverImgUrl: '../../image/dxx.png'
+}, {
+  dataUrl: 'http://stream.qqmusic.tc.qq.com/132636799.mp3',
+  name: '演员',
+  mid: "001Qu4I30eVFYb",
+  singer: '薛之谦',
+  coverImgUrl: '../../image/dxx.png'
 }]
 var _gameList=[{
     dataUrl:'http://stream.qqmusic.tc.qq.com/137192078.mp3',
@@ -61,7 +79,13 @@ var _gameList=[{
     mid: "000E62Tc3bMiJB",
     singer:'薛之谦',
     coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R90x90M000000aWdOx24i3dG.jpg'
-}]
+  }, {
+    dataUrl: 'http://stream.qqmusic.tc.qq.com/138549169.mp3',
+    name: '你还要我怎样',
+    mid: "000E62Tc3bMiJB",
+    singer: '薛之谦',
+    coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R90x90M000000aWdOx24i3dG.jpg'
+  }]
 Page({
     data:{
         content:"222",
